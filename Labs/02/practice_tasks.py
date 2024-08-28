ff753631553dc1c65078fcaf083025f19452f4a6
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2024-08-28 09:46:40 +0500

    Update practice_tasks.py

--- Labs/02/practice_tasks.py
+++ Labs/02/practice_tasks.py
@@ -28,11 +28,10 @@ def user_help():
     print("Hello!", user_name)
     choice = input("I hope you are fine, let me know how I can help you!\n")
 
-    if choice == "yes":
-        problem = input("share your problem with us\n")
-        print("Thanks for your feedback, we will resolve your problem")
-        
-    print("Okay! Good to see you, stay connected".center(50))
+    if choice != "yes":
+        print("Okay! Good to see you, stay connected".center(50))
+    problem = input("share your problem with us\n")
+    print("Thanks for your feedback, we will resolve your problem")
 user_help()
 
 # task 2
