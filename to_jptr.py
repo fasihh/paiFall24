e31d7ccd75d00a827c26c3d0be843a50b7240f3e
fasihh <fhk.tcs@gmail.com>
2024-08-31 22:40:10 +0500

    - Using os.path for paths - Added type hints - Added error handling for invalid lab

--- to_jptr.py
+++ to_jptr.py
@@ -6,12 +6,12 @@ class Config:
         self._config: dict[str, str] | None = None
         self._run()
 
-    def __getitem__(self, field: str):
+    def __getitem__(self, field: str) -> str | None:
         if self._config is None:
             return None
         return self._config[field]
 
-    def _run(self):
+    def _run(self) -> None:
         try:
             with open(self._config_file):
                 pass
@@ -24,7 +24,7 @@ class Config:
             with open(self._config_file, 'r') as f:
                 self._config = dict(map(lambda x: x.split('='), f.read().rstrip().split('\n')))
 
-    def _get_init(self):
+    def _get_init(self) -> list[tuple[str, str]]:
         if self._config is not None:
             return
 
@@ -67,43 +67,47 @@ class Config:
 
 class Converter:
     def __init__(self, std_id: str, lab_no: str, config: Config):
-        self._std_id = std_id
-        self._lab_no = lab_no
-        self._config = config
-        self._lab_dir = ""
-        self._files_content = []
+        self._std_id: str = std_id
+        self._lab_no: str = lab_no
+        self._config: Config = config
+        self._lab_dir: str = ""
+        self._files_content: list[tuple[str, str]] = []
         self._run()
 
-    def _set_lab_dir(self):
+    def _set_lab_dir(self) -> None:
         for _, dirs, _ in os.walk(self._config['labs_path']):
             for i, dir in enumerate(fm.filter(dirs, self._config['sub_labs_regex'])):
                 if int(self._lab_no)-1 != i:
                     continue
-                self._lab_dir = f"{self._config['labs_path']}\\{dir}"
+                self._lab_dir = os.path.join(self._config['labs_path'], dir)
                 break
 
-    def _read_file(self, file: str):
-        with open(f"{self._lab_dir}\\{file}", 'r') as f:
+        if not self._lab_dir:
+            raise ValueError(f"No matching directory found for lab number {self._lab_no}")
+
+    def _read_file(self, file: str) -> str:
+        with open(os.path.join(self._lab_dir, file), 'r') as f:
             return f.read().rstrip()
 
-    def _create_notebook(self):
+    def _create_notebook(self) -> None:
         notebook = nbf.v4.new_notebook()
         for file_name, content in self._files_content:
             notebook.cells.append(nbf.v4.new_markdown_cell(f"### {file_name.split('.')[0]}"))
             notebook.cells.append(nbf.v4.new_code_cell(content))
 
-        with open(f"{self._lab_dir}\\{self._std_id}_{self._lab_no:0>2}.ipynb", 'w') as f:
+        output_file = os.path.join(self._lab_dir, f"{self._std_id}_{self._lab_no:0>2}.ipynb")
+        with open(output_file, 'w') as f:
             nbf.write(notebook, f)
-        print(f"Created notebook: {self._lab_dir}\\{self._std_id}_{self._lab_no:0>2}.ipynb")
+        print(f"Created notebook: {output_file}")
 
-    def _run(self):
+    def _run(self) -> None:
         self._set_lab_dir()
         for _, _, files in os.walk(self._lab_dir):
             for file in fm.filter(files, self._config['file_regex']):
                 self._files_content.append((file, self._read_file(file)))
         self._create_notebook()
 
-def main():
+def main() -> None:
     if len(sys.argv)-1 < 2:
         print("Usage: py to_jptr.py <student_id> <lab_number>\nExample: python to_jptr.py 23K0727 1")
         print("\nAlso make sure this script is in the base directory", end='')
