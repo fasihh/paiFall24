6d84e8fdd8ead69f378481d0fe68d9355b42590c
Fasih Hasan <47947561+fasihh@users.noreply.github.com>
2024-09-18 09:30:09 +0500

    Update q03.py

--- Labs/05/q03.py
+++ Labs/05/q03.py
@@ -1,7 +1,3 @@
-# Write a program in which a class named Account has private member variables named
-# account_no ,account_bal ,security_code. Use a public function to initialize the variables and
-# print all data.
-
 class Account:
     def __init__(self, account_no, account_bal, security_code):
         self.__account_no = account_no
